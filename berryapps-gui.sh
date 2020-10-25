977477ff779a50751b65c9ee53b2e61b64b36dd1
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-10-25 11:41:53 +0200

    added gui script (still in development)

---
+++ berryapps-gui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yad --list --center --width=800 --height=600 --window-icon=/home/pi/Downloads/ssr-icon.png \
+--title "berryapps" \
+--column "app name" --column "description" \
+SimpleScreenRecorder " powerful feature packed yet simple and easy to use screen recorder" \
+System-Tools "system maintanance commands all done for you"
+
+
+
