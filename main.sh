70065892aff8d82df6a0ca75a6b3385595d43550
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-01-12 09:02:33 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+#first flag - install or remove
 if [[ $1 = "install" ]]; then
 	echo "installing..."
+elif [[ $1 = "remove" ]]; then
+	echo "removing..."
 fi
 
+#second variable - compile or precompiled
 if [[ $2 = "-c" ]]; then
 	echo "compiling from source..."
 elif [[ $2 = "-p" ]]; then
 	echo "installing pre-compiled version..."
 fi
 
+if [[ $2 = "-pa" ]]
+	echo "123"
+fi
+
 if [[ $3 = "box86" ]]; then
 	PACKAGE="box86"
-	echo "searching for box86"
+elif [[ $3 = "balena-etcher" ]]; then
+	PACKAGE="balena-etcher"
+fi
+
+if [[ $1 == "install" ]]; then
+	echo "installing $PACKAGE"
+elif [[ $1 == "remove" ]]; then
+	echo "removing $PACKAGE"
 fi
 
