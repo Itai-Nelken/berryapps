5cdf20b55a67798f43e8600888e4cf488f077d50
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-03-25 15:08:08 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,16 +10,16 @@ function help() {
     #echo -e "${underline}${light_green}pi-apps [option]${normal}"
     echo -e "\n${inverted}${bold}${light_blue}Available options:${normal}"
     echo '-------------------'
-    #echo -e "${dark_grey_background}install '[appname]'${normal} - install any app available in pi-apps, you can also use ${dark_grey_background}uninstall${normal}.\n"
-    #echo -e "${dark_grey_background}remove '[appname]'${normal} - uninstall any app available in pi-apps.\n"
-    #echo -e "${dark_grey_background}list-all${normal} - print all apps available in pi-apps.\n"
-    #echo -e "${dark_grey_background}list-installed${normal} - print all installed apps.\n"
-    #echo -e "${dark_grey_background}search '[appname]'${normal} - search all apps available in pi-apps (case sensitive).\n"
-    #echo -e "${dark_grey_background}update-all${normal} - update all pi-apps components.\n"
+    echo -e "${dark_grey_background}install '[appname]'${normal} - install a precompiled version of any app available on berryapps (case sensitive).\n"
+    echo -e "${dark_grey_background}remove '[appname]'${normal} - uninstall any app available in berryapps (case sensitive).\n"
+    echo -e "${dark_grey_background}list-all${normal} - print all apps available in berryapps.\n"
+    echo -e "${dark_grey_background}list-installed${normal} - print all installed apps.\n"
+    echo -e "${dark_grey_background}search '[appname]'${normal} - search all apps available in berryapps (case sensitive).\n"
+    echo -e "${dark_grey_background}update-all${normal} - update berryapps.\n"
     #echo -e "${dark_grey_background}update${normal} - update all apps.\n"
     #echo -e "${dark_grey_background}website '[appname]'${normal} - print the website of any app in pi-apps.\n"
     #echo -e "${dark_grey_background}gui${normal} - launch the pi-apps GUI.\n"
-    #echo '===================='
+    echo '===================='
 
     #echo -e "\n${cyan}${bold}if you don't supply any option or the option you entered is invalid,"   
     #echo -e "pi-apps will start with the GUI${normal}" 
@@ -28,8 +28,6 @@ function help() {
 
 
 ##where things actually happen##
-
-#Get the
 
 if [[ "$1" == "help" ]] || [[ "$1" == "-help" ]] || [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
     #display help
